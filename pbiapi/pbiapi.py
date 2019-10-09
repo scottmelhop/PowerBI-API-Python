162799bda78d462d5d59b6b21bd190557549a2f6
Magnus Moan <magnus.moan@cognite.com>
2019-10-09 17:14:02 +0200

    Name instead of id for dataset delete

--- pbiapi/pbiapi.py
+++ pbiapi/pbiapi.py
@@ -199,8 +199,12 @@ class PowerBIAPIClient:
             self.force_raise_http_error(response, expected_codes=[201, 202])
 
     @check_token
-    def delete_dataset(self, workspace_name: str, dataset_id: str) -> None:
+    def delete_dataset(self, workspace_name: str, dataset_name: str) -> None:
         workspace_id = self.find_entity_id_by_name(self.workspaces, workspace_name, "workspace", raise_if_missing=True)
+
+        datasets = self.get_datasets_in_workspace(self.workspaces)
+        dataset_id = self.find_entity_id_by_name(datasets, dataset_name, "dataset", raise_if_missing=True)
+
         url = self.base_url + f"groups/{workspace_id}/datasets/{dataset_id}"
         response = requests.delete(url, headers=self.headers)
         if response.status_code == HTTP_OK_CODE:
