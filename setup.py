5a01b7c93371aa951c8bba65255eb10494e6ca68
Magnus Moan <magnus.moan@cognite.com>
2019-11-12 11:14:17 +0100

    Including dependencies in deployment package

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt") as fh:
+    install_requires = fh.read().splitlines()
+
 setuptools.setup(
     name="pbiapi",
     version="0.1.4a3",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/scottmelhop/PowerBI-API-Python",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
